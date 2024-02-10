b5d6661247a37188c366113b0525ed41f76a91b9
ROUSenghak <105089856+ROUSenghak@users.noreply.github.com>
2024-02-10 23:07:45 +0100

    solver.py

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,9 +1,13 @@
 from grid import Grid
 
-class Solver(): 
-    """
-    A solver class, to be implemented.
-    """
+class Solver():
+
+    # TODO: implement this function (and remove the line "raise NotImplementedError").
+    def __init__(self, grid):
+        """
+        Initializes the solver with a grid object.
+        """
+        self.grid = grid
     
     def get_solution(self):
         """
@@ -12,5 +16,23 @@ class Solver():
         """
         # TODO: implement this function (and remove the line "raise NotImplementedError").
         # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
-        raise NotImplementedError
+        
+        solution = []  # Initialize the list to store the swaps
+        
+        m, n = self.grid.m, self.grid.n
+        
+        # Iterate through each cell in the grid
+        for i in range(m):
+            for j in range(n):
+                # Calculate the correct position for the current value
+                correct_i = (self.grid.state[i][j] - 1) // n
+                correct_j = (self.grid.state[i][j] - 1) % n
+                
+                # If the current cell is not in its sorted position
+                if (i, j) != (correct_i, correct_j):
+                    # Swap the current cell with the cell in its correct position
+                    solution.append(((i, j), (correct_i, correct_j)))
+                    self.grid.swap((i, j), (correct_i, correct_j))
+        
+        return solution
 
