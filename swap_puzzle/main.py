acfc760cee32ba56ab58fe93a32c77ae90ff7206
gdelloye-ensae <guy.delloye@ensae.fr>
2024-03-10 21:19:46 +0000

    CR en cours 2

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -34,71 +34,11 @@ def apply_bfs_to_grid(g):
     return Graph.bfs(gra, src, dst)
 
 
-def apply_bfs_to_grid_old_with_int(g):
-    """
-    Returns the list of the optimal swaps to solve the g grid
-    """
-    gra = graph_from_grid(g)
-    dico =[0 for i in range(gra.nb_nodes)]
-    for k in range(len(dico)):
-        dico[k] = gra.nodes[k]
-    #print("dico :", dico)
 
-    def index(node):
-            for i in range(len(dico)):
-                if dico[i] == node:
-                    return i
-    
-    gra_renumbered = Graph([i for i in range(len(dico))])
-    for edge in gra.edges:
-        n1, n2 = edge
-        Graph.add_edge(gra_renumbered, index(n1), index(n2))
-    #print("gra :", gra)
-    #print("gra_renumbered :", gra_renumbered)
-    src = index(int(Grid.grid_to_string(g)))
-    dst = index(min(dico))
-    #print(dico[dst], dst)
-    b = Graph.bfs(gra_renumbered, src, dst)
-    #print(b)
-    for k in range(len(b)):
-        b[k] = dico[b[k]]
-    return b
-
-
-
-#Test BFS: it worked with the int version
+#Test BFS
 g = Grid.grid_from_file("ensae-prog24/input/grid0.in")
 path = apply_bfs_to_grid(g)
 print(path)
-
-
-
-
-def a_star_remis_dans_classe_Graph(gra, src, dst, n, m):
-        """
-        Finds a shortest path in Graph gra from src to dst by A-star algorithm.
-        """
-        closed = {}
-        prec_nodes = {}
-        open_list = []
-        heapq.heappush(open_list, (Graph.distance(src), src, 0))
-        if dst == src:
-            return [src]
-        closed[src] = []
-        prec_nodes[src] = src
-        compteur = 0
-        while open_list != []:
-            compteur += 1
-            heuristic, node, cost = heapq.heappop(open_list)
-            if node == dst:
-                return closed[prec_nodes[node]] + [node]
-            for nghbr in gra.graph[node]:
-                cost = cost + 1
-                if (nghbr not in closed) and (not Graph.exists_inf(nghbr, cost, open_list)):
-                    heapq.heappush(open_list, (cost + Graph.distance(nghbr), nghbr, cost))
-                    prec_nodes[nghbr] = node
-            closed[node] = closed[prec_nodes[node]] + [node]
-        raise Exception('No path')
 
 def apply_a_star_to_grid(g):
     """
@@ -113,13 +53,13 @@ def apply_a_star_to_grid(g):
 
 
 
-"""
-#Test A_star : OK
+
+#Test A_star
 g = Grid.grid_from_file("ensae-prog24/input/grid0.in")
 path = apply_a_star_to_grid(g)
 print(path)
 
-
+"""
 g = Grid.grid_from_file("ensae-prog24/input/grid0.in")
 print("Initial:", g)
 
@@ -162,6 +102,7 @@ print(Graph.is_connected(gra0, 1, 3))
 print(g)
 print(graph_from_grid(g))
 """
+"""
 g = Grid(2,2)
 print(g)
 print("is_sorted test:")
@@ -181,3 +122,4 @@ print("After swap_freq: ", g)
 print("is_sorted test:")
 g = Grid.is_sorted(g)
 print(g)
+"""
